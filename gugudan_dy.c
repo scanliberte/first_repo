f0fff4ccb6dd9ac11006fd99f00593c4d3105bf2
DY <scanliberte@nate.com>
2016-01-24 00:38:35 +0900

    Update gugudan_dy.c

--- gugudan_dy.c
+++ gugudan_dy.c
@@ -3,8 +3,8 @@
 void gugudan_dy() {
   int i=0, k=0;
   
-  for(i=2; i<10; i++) { //for1
-    for(k=1; k<10; k++) { //for1_1
+  for(i=1; i<10; i++) { //for1
+    for(k=START; k<=END; k++) { //for1_1
       printf("%2d x%2d=%3d\t", k, i, k*i);
     }//end for1_1
     printf("\n");
